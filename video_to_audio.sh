46a5089c9f1c3a24acfe945a2f9d17a1f6799e3c
KitaharaMugiro <ngthornym@yahoo.co.jp>
2019-10-30 20:17:01 +0900

    fix

--- video_to_audio.sh
+++ video_to_audio.sh
@@ -1,6 +1,6 @@
 filename_mp4=Lesson3_20191030
 export GOOGLE_APPLICATION_CREDENTIALS=./abceed-app-f959c792ae4b.json
-echo filename_mp4
+echo $filename_mp4
 echo $GOOGLE_APPLICATION_CREDENTIALS
 
 ffmpeg -i resources/$filename_mp4.mp4 -ar 16000 resources/$filename_mp4.flac
